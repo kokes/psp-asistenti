6d48cde75c27d89b584e62bd8c05873ab3a021ce
Ondrej Kokes <ondrej.kokes@gmail.com>
2022-01-05 09:39:53 +0100

    ale no tak už

--- dl.py
+++ dl.py
@@ -4,16 +4,17 @@ import requests
 import lxml.html
 
 base_url = "https://www.psp.cz/sqw/hp.sqw?k=192"
+HTTP_TIMEOUT = 30
 
 if __name__ == "__main__":
-    r = requests.get(base_url, timeout=15)
+    r = requests.get(base_url, timeout=HTTP_TIMEOUT)
     r.raise_for_status()
 
     data = []
     ht = lxml.html.fromstring(r.text)
     for link in ht.cssselect("ul.person-list li span a"):
         person_url = urljoin(base_url, link.attrib["href"])
-        rp = requests.get(person_url, timeout=15)
+        rp = requests.get(person_url, timeout=HTTP_TIMEOUT)
         rp.raise_for_status()
         htp = lxml.html.fromstring(rp.text)
         name = htp.cssselect("h1")[0].text.replace("\xa0", " ")
