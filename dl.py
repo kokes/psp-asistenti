00b6bc4612f3f8d0aa1c96322fa369ac60e5fb18
Ondrej Kokes <ondrej.kokes@gmail.com>
2025-10-09 15:07:42 +0200

    Zjednodušení (#4)
    * odmazat neposlance
    
    * CSV only

--- dl.py
+++ dl.py
@@ -1,5 +1,5 @@
 import argparse
-import json
+import csv
 import os
 import random
 import ssl
@@ -14,7 +14,7 @@ base_url = "https://www.psp.cz/sqw/hp.sqw?k=192"
 HTTP_TIMEOUT_BASE = 30
 HTTP_TIMEOUT = 300
 
-JSON_FILENAME = "asistenti.json"
+CSV_FILENAME = "asistenti.csv"
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -25,14 +25,16 @@ if __name__ == "__main__":
         ht = lxml.html.parse(r).getroot()
 
     data = []
-    if os.path.exists(JSON_FILENAME):
-        with open(JSON_FILENAME, "rt", encoding="utf-8") as f:
-            data = json.load(f)
+    if os.path.exists(CSV_FILENAME):
+        with open(CSV_FILENAME, "rt", encoding="utf-8") as f:
+            cr = csv.DictReader(f)
+            data = list(cr)
 
     links = ht.cssselect("ul.person-list li span a")
-    random.shuffle(links)
-    for link in links[: args.n]:
-        person_url = urljoin(base_url, link.attrib["href"])
+    urls = [urljoin(base_url, j.attrib["href"]) for j in links]
+    data = [j for j in data if j["url"] in set(urls)]
+    random.shuffle(urls)
+    for person_url in urls[: args.n]:
         with urlopen(person_url, timeout=HTTP_TIMEOUT) as rp:
             dt = rp.read().decode("windows-1250")
             htp = lxml.html.fromstring(dt)
@@ -43,14 +45,19 @@ if __name__ == "__main__":
             for j in htp.cssselect("ul.assistants li > strong")
         ]
         data = [j for j in data if j["url"] != person_url]
-        data.append(
+        data.extend(
             {
                 "poslanec": name,
                 "url": person_url,
-                "asistenti": sorted(assistants),
+                "asistent": assistant,
             }
+            # at mame radku i kdyz nema asistenty 
+            for assistant in assistants or [""]
         )
 
-    data.sort(key=lambda x: x["url"])
-    with open(JSON_FILENAME, "wt", encoding="utf-8") as fw:
-        json.dump(data, fw, indent=2, ensure_ascii=False)
+    data.sort(key=lambda x: (x["url"], x["asistent"]))
+    with open(CSV_FILENAME, "wt", encoding="utf-8") as fw:
+        cw = csv.DictWriter(fw, fieldnames=["poslanec", "url", "asistent"])
+        cw.writeheader()
+        for row in data:
+            cw.writerow(row)
