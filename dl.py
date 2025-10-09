95377891d4afe625eeabda63ef7b508c2a9b7ae5
Ondrej Kokes <ondrej.kokes@gmail.com>
2025-10-09 15:06:07 +0200

    CSV only

--- dl.py
+++ dl.py
@@ -1,6 +1,5 @@
 import argparse
 import csv
-import json
 import os
 import random
 import ssl
@@ -15,7 +14,7 @@ base_url = "https://www.psp.cz/sqw/hp.sqw?k=192"
 HTTP_TIMEOUT_BASE = 30
 HTTP_TIMEOUT = 300
 
-JSON_FILENAME = "asistenti.json"
+CSV_FILENAME = "asistenti.csv"
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -26,9 +25,10 @@ if __name__ == "__main__":
         ht = lxml.html.parse(r).getroot()
 
     data = []
-    if os.path.exists(JSON_FILENAME):
-        with open(JSON_FILENAME, "rt", encoding="utf-8") as f:
-            data = json.load(f)
+    if os.path.exists(CSV_FILENAME):
+        with open(CSV_FILENAME, "rt", encoding="utf-8") as f:
+            cr = csv.DictReader(f)
+            data = list(cr)
 
     links = ht.cssselect("ul.person-list li span a")
     urls = [urljoin(base_url, j.attrib["href"]) for j in links]
@@ -45,22 +45,19 @@ if __name__ == "__main__":
             for j in htp.cssselect("ul.assistants li > strong")
         ]
         data = [j for j in data if j["url"] != person_url]
-        data.append(
+        data.extend(
             {
                 "poslanec": name,
                 "url": person_url,
-                "asistenti": sorted(assistants),
+                "asistent": assistant,
             }
+            # at mame radku i kdyz nema asistenty 
+            for assistant in assistants or [""]
         )
 
-    data.sort(key=lambda x: x["url"])
-    with open(JSON_FILENAME, "wt", encoding="utf-8") as fw:
-        json.dump(data, fw, indent=2, ensure_ascii=False)
-
-
-    with open("asistenti.csv", "wt", encoding="utf-8") as fw:
-        cw = csv.writer(fw)
-        cw.writerow(["poslanec", "url", "asistent"])
-        for poslanec in data:
-            for asistent in poslanec["asistenti"] or [None]:
-                cw.writerow([poslanec["poslanec"], poslanec["url"], asistent])
+    data.sort(key=lambda x: (x["url"], x["asistent"]))
+    with open(CSV_FILENAME, "wt", encoding="utf-8") as fw:
+        cw = csv.DictWriter(fw, fieldnames=["poslanec", "url", "asistent"])
+        cw.writeheader()
+        for row in data:
+            cw.writerow(row)
